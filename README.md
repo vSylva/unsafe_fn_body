187eb18f2fc204c368fa6128d899f34e9ced02fa
vSylva <58687743+vSylva@users.noreply.github.com>
2024-02-16 18:52:54 +0800

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+Wrap the function body in an unsafe block using attribute macros
+
+```rust
+#[unsafe_fn_body::unsafe_fn_body]
+fn unsafe_fn_body(){
+}
+```
---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+//! ```rust
+//! #[unsafe_fn_body::unsafe_fn_body]
+//! fn unsafe_fn_body(){
+//! }
+//! ```
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemFn};
+
+#[proc_macro_attribute]
+pub fn unsafe_fn_body(_: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemFn);
+
+    let fn_name = input.sig.ident;
+    let fn_args = input.sig.inputs;
+    let fn_output = input.sig.output;
+    let fn_body = input.block;
+    let fn_vis = input.vis;
+    let fn_abi = input.sig.abi;
+    let fn_generics = input.sig.generics;
+    let fn_asyncness = input.sig.asyncness;
+
+    let expanded = quote! {
+       #fn_vis #fn_asyncness #fn_abi fn #fn_name #fn_generics(#fn_args) #fn_output {
+            unsafe {
+                #fn_body
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
