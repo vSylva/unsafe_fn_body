0da7a78f70a41a54916909c07a8b6eaed24429d6
vSylva <58687743+vSylva@users.noreply.github.com>
2024-03-01 02:08:16 +0800

    Update to 0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ use syn::{parse_macro_input, ItemFn};
 
 #[proc_macro_attribute]
 pub fn unsafe_fn_body(_: TokenStream, item: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(item as ItemFn);
+    let input: ItemFn = parse_macro_input!(item);
 
     let fn_name = input.sig.ident;
     let fn_args = input.sig.inputs;
@@ -22,11 +22,9 @@ pub fn unsafe_fn_body(_: TokenStream, item: TokenStream) -> TokenStream {
     let fn_asyncness = input.sig.asyncness;
 
     let expanded = quote! {
-       #fn_vis #fn_asyncness #fn_abi fn #fn_name #fn_generics(#fn_args) #fn_output {
-            unsafe {
-                #fn_body
-            }
-        }
+       #fn_vis #fn_asyncness #fn_abi fn #fn_name #fn_generics(#fn_args) #fn_output{
+            unsafe #fn_body
+       }
     };
 
     TokenStream::from(expanded)
